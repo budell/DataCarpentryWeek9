0a948bdba3f6853168bc36388dca09f026139216
Brad Udell <bradjudell@ufl.edu>
2015-10-30 13:19:20 -0400

    First, i added an output folder to store the .csv in.
    Next, i wrote some code to use the functions to get the ear class, calculate the gc content
    for each individual, select the relevant collumns, and save it as a .csv file in the output folder

--- houseelf-analysis.R
+++ houseelf-analysis.R
@@ -1,5 +1,6 @@
 # this analysis is conducted for H. Granger, anlayzing the GC content by ear length for house elves
 library(stringr)
+library(dplyr)
 
 # import the data
 get_data <- function(){
@@ -26,3 +27,22 @@ get_size_class <- function(seq){
    ear_lengths <- ifelse(seq > 10, "large", "small")
    return(ear_lengths)
 }
+
+
+### here is a code the uses the functions to import the data, get the ear class, calculat the gc content
+# for each individual, selects the relevant collumns, and saves it as a .csv file in the output folder
+data <-get_data()
+
+data_ear_class <- data %>% 
+  mutate(ear_class = get_size_class(earlength))
+
+
+data_ear_gc <- data_ear_class  %>% 
+  mutate(GC = calc_gc(dnaseq))
+
+
+data_store <- data_ear_gc %>%
+  select(id,ear_class,GC)
+
+write.csv(data_store,"output/grangers_analysis.csv")
+
