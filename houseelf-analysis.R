f9f8fbd6c60d71bce1f4b4a4ac165622fac8c962
Brad Udell <bradjudell@ufl.edu>
2015-10-30 12:26:58 -0400

    I added a function to calcuate the gc content in vectorize form, which can use both upper and lower case.

--- houseelf-analysis.R
+++ houseelf-analysis.R
@@ -1,4 +1,5 @@
 # this analysis is conducted for H. Granger, anlayzing the GC content by ear length for house elves
+library(stringr)
 
 # import the data
 get_data <- function(){
@@ -7,5 +8,16 @@ get_data <- function(){
 }
 
 data <-get_data()
+
+
+#function to calculate gc content in upper or lower case, and vectorized
+
+calc_gc <- function(sequence){
+  #this function uses stringr to calculat the G or C content of a DNA string 
+  Gs <- str_count(sequence, ('G')) + str_count(sequence, ('g'))
+  Cs <- str_count(sequence, ('C')) + str_count(sequence, ('c'))
+  gc_content <- (Gs + Cs) / str_length(sequence) * 100 
+  return(gc_content)
+}
 
 
