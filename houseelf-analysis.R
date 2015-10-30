7385b8d1e39cc85f311ec1c66699517b17de1f37
Brad Udell <bradjudell@ufl.edu>
2015-10-30 12:10:13 -0400

    Added code that imports the data

--- houseelf-analysis.R
+++ houseelf-analysis.R
@@ -1,1 +1,9 @@
 # this analysis is conducted for H. Granger, anlayzing the GC content by ear length for house elves
+
+# import the data
+get_data <- function(){
+  data <- read.csv(file = "data/houseelf_earlength_dna_data.csv")
+  return(data)
+}
+
+data <-get_data()
